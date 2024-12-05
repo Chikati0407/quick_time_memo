9c0849ec284cf4d8f277020f3e2e698ac2f4e84a
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2024-12-05 14:28:32 +0900

    index.js少しいじる
    イマイチわかっていない

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,47 @@
 // Cloud Functions 第2世代（公式で推奨）
-const functions = require("firebase-functions/v2");
+const functions = require("firebase-functions/v2")
 
-// 外部からアクセスできる関数を定義
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  // Firebaseのログに出力
-  functions.logger.info("Hello logs!", {structuredData: true});
-  // レスポンスとして値を返す
-  response.send("Hello from Firebase!");
+// firebase-adminからfirestoreを代入
+const admin = require("firebase-admin")
+
+// firestore初期化
+admin.initializeApp()
+const firestore = admin.firestore()
+const msg = admin.messaging()
+
+// push通知メソッド
+const pushMessage = (fcmToken, text) => ({
+  notification: {
+      title: '新しいオファーを受信しました。',
+      body:  `${text}`,
+  },
+  apns: {
+      headers: {
+          'apns-priority': '10'
+      },
+      payload: {
+          aps: {
+              badge: 9999,
+              sound: 'default'
+          }
+      }
+  },
+  data: {
+      data: 'test',
+  },
+  token: fcmToken
 });
+
+
+// 関数
+exports.mySendMeaasge = functions.region('asia-northeast1')
+  .runWith({memory: "512MB"})
+  .pubsub.schedule("every 5 minutes")
+  .timeZone("Asia/Tokyo")
+  .onRun(async (context) => {
+
+    
+  });
+  
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 
 // Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 // Package imports:
 import 'package:firebase_core/firebase_core.dart';
@@ -14,7 +13,6 @@ import 'package:time_memo_app/pages/to_do_page.dart';
 import 'package:time_memo_app/theme.dart';
 import 'package:time_memo_app/widgets/bottom_app_bar.dart';
 import 'package:time_memo_app/widgets/floatingAcbu.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -31,56 +29,48 @@ void main() async {
   // debugPrint('FCM TOKEN: $fcmToken');
 
   runApp(
-    ProviderScope(
-      child: MyToDoApp()
-    ),
+    ProviderScope(child: MyToDoApp()),
   );
 }
-
 
 class MyToDoApp extends ConsumerWidget {
   const MyToDoApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     return MaterialApp(
       // themeMode: (ref.read(test_mode_provider)) ? ThemeMode.dark : ThemeMode.light,
-      theme : MaterialTheme().light(),
+      theme: MaterialTheme().light(),
       darkTheme: MaterialTheme().dark(),
       home: MyPage(),
     );
   }
 }
 
-
 class MyPage extends ConsumerWidget {
   const MyPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     // final visible_appbar = ref.watch(bottom_appbar_provider);
 
     var c1 = Theme.of(context).colorScheme.surface;
     var c2 = Theme.of(context).colorScheme.onSurface;
-    final width=MediaQuery.of(context).size.width;
-    final height=MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: c1,
       floatingActionButton: NeuFloatingActionbutton(),
       body: CustomPaint(
-        size: Size(width,height),
-        painter: DotPainter(
-          dotColor: c2,
-          dotRadius: 1,
-          spacing: 10,
-        ),
-        child: ToDoPage()
-      ),
+          size: Size(width, height),
+          painter: DotPainter(
+            dotColor: c2,
+            dotRadius: 1,
+            spacing: 10,
+          ),
+          child: ToDoPage()),
       // floatingActionButtonLocation: (visible_appbar) ? FloatingActionButtonLocation.endContained : FloatingActionButtonLocation.endFloat,
       bottomNavigationBar: NeuBottomAppBar(),
     );
   }
 }
-
