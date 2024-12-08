c7c4ac9a3eab43c6f11ecc351fb9e1e2d9d4e04b
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2024-12-08 13:55:34 +0900

    firebase functionを使って通知機能実装

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions/v1");
 const admin = require("firebase-admin");
+
 const { Timestamp } = require("firebase-admin/firestore");
 
 admin.initializeApp();
@@ -8,30 +9,17 @@ const firestore = admin.firestore();
 const fcm_token = "cINKbFkHQYiglejP6zo3eM:APA91bEWJeyU03fdetCDX9JEif5RBHNUoUkLoWJaViokwrknf4vbn9IKygw7ByLSnzXFoL-9RvPxikvl6yCZmhpLsOby0RZmB0IF0eBnlkdbfwgqP8848L0";
 
 
-// exports.scheduledFunction = functions.pubsub.schedule("every 5 minutes").onRun( async (context)  => {
-//   const now = Date.now();
-//   const snapshot = await firestore.collection("todo").get();
-
-//   snapshot.docs.forEach(async (doc) => {
-//     const data = doc.data();
-//     console.log(data);
-//   });
-
-
-//   return null;
-// });
-
-
-exports.helloWorld = functions.https.onRequest(async (request, response) => {
+exports.scheduledFunction = functions.pubsub.schedule("every 5 minutes").onRun( async (context)  => {
   const now = Date.now();
-  const now5 = now + 300;
+  const now5 = now + 300000;
+
   const snapshot = await firestore.collection("todo").get();
 
   snapshot.docs.forEach(async (doc) => {
     const data = doc.data();
-    const time = data["time"].toDate();
+    const time = Number(data.time.seconds) * 1000;
 
-    if (now < time){
+    if (now < time && time < now5){
 
       const pushMSG = {
         notification: {
@@ -56,9 +44,49 @@ exports.helloWorld = functions.https.onRequest(async (request, response) => {
       admin.messaging().send(pushMSG);
 
     }
-
-    
   });
+});
+
+
+// exports.testFunction = functions.https.onRequest(async (request, response) => {
+//   const now = Date.now();
+//   const now5 = now + 300000;
+
+
+
+//   const snapshot = await firestore.collection("todo").get();
+
+//   snapshot.docs.forEach(async (doc) => {
+//     const data = doc.data();
+//     const time = Number(data.time.seconds) * 1000;
+
+
+//     if (now < time && time < now5){
+
+//       const pushMSG = {
+//         notification: {
+//           title: `${data["title"]}`,
+//           body:  `${data["content"]}`,
+//         },
+//         apns: {
+//           payload: {
+//               aps: {
+//                   sound: 'default'
+//               }
+//           }
+//         },
+//         data: {
+//           data: 'test',
+//         },
+  
+//         token: fcm_token,
+    
+//       }
+  
+//       admin.messaging().send(pushMSG);
+
+//     }
+//   });
 
   
-});
+// });
